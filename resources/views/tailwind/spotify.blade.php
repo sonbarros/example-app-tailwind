be1033e3f4d61d7f8335e37679cc5f2c1273887f
Anderson Barros <anderson21br@gmail.com>
2021-07-02 04:20:22 +0000

    installed tailwindcss

--- resources/views/tailwind/spotify.blade.php
+++ resources/views/tailwind/spotify.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!-- Import Tailwind CSS -->
     <title>Document</title>
 </head>
 <body>
     
-    
+    <h1 class="bg-red-400 m-4">Teste</h1>
+    <i class="fab fa-instagram"></i>
 </body>
 </html>
